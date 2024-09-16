49d2610a445ed66185667791e1d88a1df2224527
bunny <22am1a3118@svrec.ac.in>
2024-09-16 01:55:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var typed = new Typed('.typed', {
+    strings: [" Developer ...", "Front end ...", "Programmer ..."],
+    typeSpeed: 100,
+    backDelay:2000,
+    smartBackspace: true,
+    loop: true,
+    backDelay: 1500
+});
+
+
