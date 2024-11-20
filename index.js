a42c6dc35d4ea9cb92a7f75ec1920ad654880dd1
Bhanuteja59 <144136452+Bhanuteja59@users.noreply.github.com>
2024-11-20 23:15:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 var typed = new Typed('.typed', {
-    strings: [" Developer ...", "Front end ...", "Programmer ..."],
+    strings: ["Developer...", "Front end...", "Programmer..."],
     typeSpeed: 100,
     backDelay:2000,
     smartBackspace: true,
     loop: true,
     backDelay: 1500
 });
-
-
